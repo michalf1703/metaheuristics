f8784dcfe706b4ac87773c9036c7496a3e9150bd
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-11-04 14:59:51 +0100

    save best solution

--- zad1/main.py
+++ zad1/main.py
@@ -1,6 +1,12 @@
 import random
 import math
 
+# Parametry początkowe
+T = 500
+alpha = 0.999 * T
+k = 0.1
+M = 3000
+
 def f(x):
     if -105 < x < -95:
         return -2 * abs(x + 100) + 10
@@ -15,8 +21,10 @@ def simulated_annealing(T, alpha, k, M):
     best_solution = current_solution
     best_cost = current_cost
 
+    sB = best_solution
+
     for i in range(M):
-        new_solution = current_solution + random.uniform(-5, 5)  # Generowanie sąsiedniego rozwiązania
+        new_solution = current_solution + random.uniform(0, 1)  # Generowanie sąsiedniego rozwiązania
         new_solution = max(-150, min(new_solution, 150))  # Ograniczenie rozwiązania do przedziału <-150, 150>
         new_cost = f(new_solution)
 
@@ -26,19 +34,18 @@ def simulated_annealing(T, alpha, k, M):
             current_solution = new_solution
             current_cost = new_cost
 
-        if current_cost > best_cost:
-            best_solution = current_solution
-            best_cost = current_cost
+        if new_cost > best_cost:
+            best_solution = new_solution
+            best_cost = new_cost
+
+        # Aktualizacja najlepszego rozwiązania od początku
+        if new_cost < f(sB):
+            sB = new_solution
 
         T = alpha * T
 
     return best_solution, best_cost
 
-# Parametry początkowe
-T = 500
-alpha = 0.999 * T
-k = 0.1
-M = 3000
 
 best_solution, best_cost = simulated_annealing(T, alpha, k, M)
 
