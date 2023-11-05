4c803049fd70c2d67e248bd76fc1201ada76bc74
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-11-05 03:30:49 +0100

    update menu and add initial documentation

--- zad1/main.py
+++ zad1/main.py
@@ -18,10 +18,8 @@ def simulated_annealing(T, alpha, k, M, f,s1,s2,r1,r2):
     best_solution = current_solution
     best_cost = current_cost
 
-    sB = best_solution
-
     for i in range(M):
-        new_solution = current_solution + random.uniform(r1, r2)  # Rozszerzenie obszaru sąsiedztwa
+        new_solution = current_solution + random.uniform(r1, r2)
         new_solution = max(s1, min(new_solution, s2))
 
         new_cost = f(new_solution)
@@ -35,49 +33,48 @@ def simulated_annealing(T, alpha, k, M, f,s1,s2,r1,r2):
             best_solution = new_solution
             best_cost = new_cost
 
-        # Aktualizacja najlepszego rozwiązania od początku
-        if new_cost < f(sB):
-            sB = new_solution
-
         T = alpha * T
 
     return best_solution, best_cost
 
 def menu():
-    print("Wybierz funkcję do optymalizacji:")
-    print("1. f(x) = x * sin(10πx) + 1")
-    print("2. f(x) = -2 * abs(x + 100) + 10")
+    while True:
+        print("__________________________________________________________________________________________")
+        print("Wybierz funkcję do optymalizacji:")
+        print("1. f(x) = x * sin(10πx) + 1 , dla przedziału [-2,1]")
+        print("2. f(x) = -2 * |x + 100) + 10 dla x należącego do (-105, -95)")
+        print("   f(x) = -2.2 * |x - 100| + 11 dla x należącego do (95, 105)")
+        print("   f(x) = 0 dla reszty x z przedziału [-150,150]")
+        print("Inna komenda - zakończenie pracy programu")
+        choice = input("Podaj numer funkcji (1 lub 2): ")
+        if choice == "1":
+            T = 5
+            alpha = 0.997 * T
+            k = 0.1
+            M = 1200
+            s1 = -1
+            s2 = 2
+            r1 = -0.1
+            r2 = 0.1
+            result = simulated_annealing(T, alpha, k, M, f1, s1, s2, r1, r2)
+            function_name = "f(x) = x * sin(10πx) + 1"
+        elif choice == "2":
+            T = 500
+            alpha = 0.999 * T
+            k = 0.1
+            M = 3000
+            s1 = -150
+            s2 = 150
+            r1 = -15
+            r2 = 15
+            result = simulated_annealing(T, alpha, k, M, f2, s1, s2, r1, r2)
+            function_name = "numer 2 "
+        else:
+            print("Zakończenie pracy programu.")
+            break
 
-    choice = input("Podaj numer funkcji (1 lub 2): ")
+        best_solution, best_cost = result
+        print(f"Maksimum globalne funkcji {function_name}: x = {best_solution}, f(x) = {best_cost}")
 
-    if choice == "1":
-        T = 5
-        alpha = 0.997 * T
-        k = 0.1
-        M = 1200
-        s1 = -1
-        s2 = 2
-        r1 = -0.1
-        r2 = 0.1
-        result = simulated_annealing(T, alpha, k, M, f1, s1, s2, r1, r2)
-        function_name = "f(x) = x * sin(10πx) + 1"
-    elif choice == "2":
-        T = 500
-        alpha = 0.999 * T
-        k = 0.1
-        M = 3000
-        s1 = -150
-        s2 = 150
-        r1 = 0
-        r2 = 1
-
-        result = simulated_annealing(T, alpha, k, M, f2, s1, s2, r1, r2)
-        function_name = "f(x) = -2 * abs(x + 100) + 10"
-    else:
-        print("Nieprawidłowy wybór funkcji.")
-        return
-
-    best_solution, best_cost = result
-    print(f"Maksimum globalne funkcji {function_name}: x = {best_solution}, f(x) = {best_cost}")
 
 menu()
